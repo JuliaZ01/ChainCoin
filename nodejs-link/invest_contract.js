c2ae05d51b3da8a4a721e2845ea1a634ea31e16c
JenniferZang <zangyaxuan@gmail.com>
2019-04-21 19:20:09 +0800

    Update invest_contract.js

--- nodejs-link/invest_contract.js
+++ nodejs-link/invest_contract.js
@@ -6,15 +6,12 @@ const options = {
   host: '127.0.0.1:36002',
 };
 const sdk = new BumoSDK(options);
-// const genesisaddress = 'buQf5VAcYgWhJTp9Fywpu7W9KWfnUTK55MHt'
-// const genesispk = 'privbyrGtc3YaTLf4iWPU68Dwx83j8jjLvsoqAS3zbjVX5BcDvP7uYhE'
-const genesisaddress = 'buQBH6Puc178ZkDNzWMBaii2BPf67Y2TFdHL';
-const genesispk = 'privbwskYeUaaL4LcxvJhQqJLAswzsEnGrHaM82VZdk9nW8nVPBYPVrQ';
-const caddress = 'buQjPgvddwvtpFYf76hZHW6kWk1xaMvSG6UB';
-
-const CreateProject = async function(){
+const investContract = async function(user,pr,cad,coin){
 //获取Nonce
-	const nonceInfo = await sdk.account.getNonce(genesisaddress)
+    bcoin = coin.toString();
+    coin = coin*100000000;
+    mcoin = coin.toString();
+	const nonceInfo = await sdk.account.getNonce(user)
 	if(nonceInfo.errorCode != 0){
 		console.log(nonceInfo);
 		return;
@@ -23,16 +20,16 @@ const CreateProject = async function(){
 	nonce = new BigNumber(nonce).plus(1).toString(10);
 //构建操作
    const params = {
-       to: genesisaddress,
-       value: '5',
+       to: user,
+       value: bcoin,
    }
    const initInput = {
         params: params,
         method: 'transfer',
    }
    const operationInfo = await sdk.operation.contractInvokeByBUOperation({
-       contractAddress: caddress,
-       buAmount: '500000000',
+       contractAddress: cad,
+       buAmount: mcoin,
        input: JSON.stringify(initInput),
    });
    if(operationInfo.errorCode != 0){
@@ -42,7 +39,7 @@ const CreateProject = async function(){
     const operation = operationInfo.result.operation;
  //序列化交易
    const buildInfo = await sdk.transaction.buildBlob({
-   	   sourceAddress : genesisaddress,
+   	   sourceAddress : user,
    	   gasPrice : '1000',
    	   feeLimit : '1000000',
    	   nonce : nonce,
@@ -55,7 +52,7 @@ const CreateProject = async function(){
    const blob = buildInfo.result.transactionBlob;
 //用私钥进行签名
     const signatureInfo = sdk.transaction.sign({
-    privateKeys : [ genesispk, ],
+    privateKeys : [ pr ],
     blob : blob,
   });
    if (signatureInfo.errorCode != 0){
@@ -70,12 +67,12 @@ const CreateProject = async function(){
    });
    return submitInfo;
 };
-CreateProject().then(result =>{
-  console.log(result);
-  return result;
-})
-.catch(err =>{
-  console.log(err);
-});
+// CreateProject().then(result =>{
+//   console.log(result);
+//   return result;
+// })
+// .catch(err =>{
+//   console.log(err);
+// });
 
-module.exports.CreateProject = CreateProject;
+module.exports.investContract = investContract;
