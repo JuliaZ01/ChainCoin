b0ec3f4812c3ec1cd2040ae3a7ec92b6caa410f7
JenniferZang <zangyaxuan@gmail.com>
2019-04-21 17:04:58 +0800

    Update test.py

--- nodejs-link/test/test.py
+++ nodejs-link/test/test.py
@@ -1,5 +1,6 @@
 import redis
 import zerorpc
+import time
 # r = redis.StrictRedis(host='localhost', port=6379)
 # r.publish('CreateKeypair', 'createaccount')
 user = 'buQf5VAcYgWhJTp9Fywpu7W9KWfnUTK55MHt'
@@ -13,11 +14,11 @@ c.connect("tcp://127.0.0.1:4343")
 # address = kp[0:36]
 # pbkey = kp[36:112]
 # prkey = kp[112:]
-# hash1 = c.createproject(user, coins, prkey)
-# print(type(hash1))
-# print(type(hash1)== type('abc'))
 
-
-result = c.getcontractad('d7d27196ef98a7606a15c0361144d1c58f30c90fabaf822249386dfb4429a823')
-print(result[0]['contract_address'])
+hash1 = c.createproject(user, coins, prkey)
+print(hash1)
+time.sleep(15)
+if type(hash1)== type('abc'):
+	result = c.getcontractad(hash1)
+	print(result)
 
