bab25846fa1883896368eaf0d04447894eafb267
JenniferZang <zangyaxuan@gmail.com>
2019-04-27 20:48:54 +0800

    解决migration问题

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'invest.apps.InvestConfig',
     'accounts.apps.AccountsConfig',
     'django_apscheduler',
+    'usermana.apps.UsermanaConfig',
 ]
 
 MIDDLEWARE = [
--- usermana/models.py
+++ usermana/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from login.models import Users
 # Create your models here.
+class Blacklist(models.Model):
+    Bl_User = models.ForeignKey(Users, on_delete = models.CASCADE)
+
+class Volunteer(models.Model):
+    Vl_User = models.ForeignKey(Users, on_delete = models.CASCADE)
+    Vl_Detail = models.CharField(max_length = 1000)
