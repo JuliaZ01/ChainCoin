4bb6f5e7e9075999297b1651b5d9d8021476cdc2
JenniferZang <zangyaxuan@gmail.com>
2019-04-27 20:58:44 +0800

    彻底解决migration问题

--- usermana/models.py
+++ usermana/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from login.models import Users
 # Create your models here.
 class Blacklist(models.Model):
-    Bl_User = models.ForeignKey(Users, on_delete = models.CASCADE)
+    Bl_user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    def __str__(self):
+        return self.Bl_User
 
 class Volunteer(models.Model):
-    Vl_User = models.ForeignKey(Users, on_delete = models.CASCADE)
-    Vl_Detail = models.CharField(max_length = 1000)
+    Vl_name = models.CharField(max_length = 500)
+    Vl_user = models.ForeignKey(Users, on_delete = models.CASCADE)
+    Vl_detail = models.CharField(max_length = 1000)
+    def __str__(self):
+        return self.Vl_name
